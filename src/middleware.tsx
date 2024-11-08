a326be737cde9e910e09e8ea519f1105f649f94b
endersurya19 <endersurya19@gmail.com>
2024-11-08 18:07:55 +0800

    middleware

---
+++ src/middleware.tsx
@@ -0,0 +1,15 @@
+import { MiddlewareConfig, NextRequest, NextResponse } from "next/server";
+
+export async function middleware(request:NextRequest){
+    const token = request.cookies.get("token")?.value;
+    if(!token){
+        return NextResponse.redirect(new URL("/auth/signin",request.url))
+    }
+    else{
+        return NextResponse.next(); 
+    }
+   
+}
+export const config:MiddlewareConfig ={
+    matcher:["/","/category/:path*","/product/:path*"],
+}
